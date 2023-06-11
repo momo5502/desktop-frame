764fe10a6111a0ba744490b55fce3fb2c8ca8e3b
Maurice Heumann <mauriceheumann@gmail.com>
2023-06-11 12:39:11 +0200

    Fix video app

--- src/cef/cef_ui_popup_handler.cpp
+++ src/cef/cef_ui_popup_handler.cpp
@@ -4,8 +4,10 @@
 
 namespace cef
 {
-	void cef_ui_popup_handler::fill_window_info(CefWindowInfo&) const
+	void cef_ui_popup_handler::fill_window_info(CefWindowInfo& window_info) const
 	{
-
+		window_info.bounds.width = 900;
+		window_info.bounds.height = 500;
+		window_info.style &= ~(WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_THICKFRAME);
 	}
 }
